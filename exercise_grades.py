ee2c5846088c7085b8ed5b20f4415f15c7d92e5b
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-03-10 19:54:40 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -11,3 +11,11 @@ def grades():
     nota1 = 8
     nota2 = 7
     nota3 = 9
+    promedio =(nota1+nota2+nota3)/3
+    maximo=max(nota1,nota2,nota3)
+    minimo=min(nota1,nota2,nota3)
+    puntos=10-promedio
+    print(promedio)
+    print(maximo)
+    print(minimo)
+    print(puntos)
