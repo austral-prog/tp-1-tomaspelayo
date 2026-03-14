2723024922c7dbe69313d6aa64fff026b5a24938
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-03-14 18:33:37 -0300

    Update exercise_statistics.py

--- exercise_statistics.py
+++ exercise_statistics.py
@@ -12,3 +12,11 @@ def statistics():
     num2 = 8
     num3 = 23
     num4 = 12
+    promedio = (num1 + num2 + num3 + num4) / 4
+    maximo = max(num1, num2, num3, num4)
+    minimo = min(num1, num2, num3, num4)
+    rango = maximo - minimo
+    print(promedio)
+    print(maximo)
+    print(minimo)
+    print(rango)
