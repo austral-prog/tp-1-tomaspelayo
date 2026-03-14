7f50d3683e9c65e56c7c2dc01c6708fd0c6890eb
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-03-14 18:31:43 -0300

    Update exercise_price.py

--- exercise_price.py
+++ exercise_price.py
@@ -9,3 +9,12 @@ def price():
     4. El precio final (subtotal + propina)
     """
     precio_base = 100
+    precio_impuesto = precio_base * 0.21
+    precio_subtotal = precio_impuesto + precio_base
+    monto_propina = precio_subtotal * 0.10
+    precio_final = precio_subtotal + monto_propina
+
+    print(precio_impuesto)
+    print(precio_subtotal)
+    print(monto_propina)
+    print(precio_final)
