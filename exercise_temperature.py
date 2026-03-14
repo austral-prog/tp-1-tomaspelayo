8843b132384ab22e406500e0a7ac3532adee9d61
tomaspelayo <tfernandezpelayo@mail.austral.edu.ar>
2026-03-14 19:10:58 -0300

    Update exercise_temperature.py

--- exercise_temperature.py
+++ exercise_temperature.py
@@ -7,3 +7,8 @@ def temperature():
     2. La temperatura original en Celsius
     """
     celsius = 25
+    
+    original=celsius
+    fahrenheit = (celsius * 9 / 5) + 32
+    print(fahrenheit)
+    print(original)
